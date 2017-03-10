25915308fa477205e3fb54cc14e4b665e6f2d445
jinhwaoh <jinhwa.j.oh@gmail.com>
2017-03-10 10:24:27 -0500

    o

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	  $(".tt1").hover(function(){
+	  	$("body").css('background-image','url(https://68.media.tumblr.com/82a3f7378ffccfa9e185377b1af6b07f/tumblr_olekr0oy271u1onkxo1_1280.jpg)');
+  })
+
+	  $(".tt2").hover(function(){
+	  	$("body").css('background-image','url(https://static.wixstatic.com/media/1f3eda_6aed9ba540ce4c12816db9c9d9452adb~mv2_d_4288_2848_s_4_2.jpg/v1/fill/w_1882,h_1248,al_c,q_90,usm_0.66_1.00_0.01/1f3eda_6aed9ba540ce4c12816db9c9d9452adb~mv2_d_4288_2848_s_4_2.jpg)');
+  })
+});
